ffb1f55d595cb37f08d132d714278347a40d8666
Marc Wieser <marc.wieser@datadoghq.com>
2025-01-17 16:53:44 +0100

    Remove unused windows string replacement

--- internal/testutility/utility.go
+++ internal/testutility/utility.go
@@ -2,24 +2,10 @@ package testutility
 
 import (
 	"os"
-	"runtime"
-	"strings"
 	"testing"
 
 	"github.com/gkampitakis/go-snaps/snaps"
 )
-
-// applyWindowsReplacements will replace any matching strings if on Windows
-func applyWindowsReplacements(content string, replacements map[string]string) string {
-	if //goland:noinspection GoBoolExpressions
-	runtime.GOOS == "windows" {
-		for match, replacement := range replacements {
-			content = strings.ReplaceAll(content, match, replacement)
-		}
-	}
-
-	return content
-}
 
 // CleanSnapshots ensures that snapshots are relevant and sorted for consistency
 func CleanSnapshots(m *testing.M) {
@@ -47,32 +33,4 @@ func SkipIfNotAcceptanceTesting(t *testing.T, reason string) {
 	if !IsAcceptanceTest() {
 		Skip(t, "Skipping extended test: ", reason)
 	}
-}
-
-func ValueIfOnWindows(win, or string) string {
-	if //goland:noinspection GoBoolExpressions
-	runtime.GOOS == "windows" {
-		return win
-	}
-
-	return or
-}
-
-// CreateTestDir makes a temporary directory for use in testing that involves
-// writing and reading files from disk, which is automatically cleaned up
-// when testing finishes
-func CreateTestDir(t *testing.T) string {
-	t.Helper()
-
-	p, err := os.MkdirTemp("", "osv-scanner-test-*")
-	if err != nil {
-		t.Fatalf("could not create test directory: %v", err)
-	}
-
-	// ensure the test directory is removed when we're done testing
-	t.Cleanup(func() {
-		_ = os.RemoveAll(p)
-	})
-
-	return p
 }
