06cd0e01c99ad5f9d16bd7a84015d5adbe19f326
Daniel Strong <daniel.strong@datadoghq.com>
2025-03-31 15:15:45 -0400

    Add reachability info to models

---
+++ pkg/models/reachability.go
@@ -0,0 +1,62 @@
+package models
+
+import "encoding/json"
+
+type ReachabilityAnalysis struct {
+	PurlToReachabilityAnalysisResults PurlToReachabilityAnalysisResults
+}
+
+// PurlToReachabilityAnalysisResults is a map of purl -> ReachabilityAnalysisResults
+type PurlToReachabilityAnalysisResults map[string]*ReachabilityAnalysisResults
+
+// ReachabilityAnalysisResults contains the results of a reachability analysis for one PURL.
+type ReachabilityAnalysisResults struct {
+	ReachableVulnerabilities []ReachableVulnerability
+	AdvisoryIdsChecked       []string
+}
+
+// ReachableVulnerability contains info for a vulnerability that was deemed reachable.
+type ReachableVulnerability struct {
+	// AdvisoryId is the vulnerability identifier that was analyzed.
+	AdvisoryId string `json:"advisory_id"`
+	// Locations where the vulnerability was deemed reachable, if any.
+	ReachableSymbolLocations ReachableSymbolLocations `json:"reachable_symbol_locations,omitempty"`
+}
+
+// ReachableSymbolLocations contains all locations (and their associated symbols) where the
+// vulnerability was determined to be reachable.
+type ReachableSymbolLocations []ReachableSymbolLocation
+
+// ReachableSymbolLocation details where a vulnerability was deemed reachable.
+type ReachableSymbolLocation struct {
+	PackageLocation
+	Symbol string `json:"symbol"`
+}
+
+// MarshalToJSONString marshals the ReachableSymbolLocations list into a JSON string
+// This is needed to pass this information into a CycloneDX field that requires a string.
+func (reachableSymbolLocations ReachableSymbolLocations) MarshalToJSONString() (string, error) {
+	str, err := json.Marshal(reachableSymbolLocations)
+	if err != nil {
+		return "", err
+	}
+
+	return string(str), nil
+}
+
+type AdvisoriesToCheckPerLanguage map[string][]AdvisoryToCheck
+
+type AdvisoryToCheck struct {
+	Purl       string
+	AdvisoryId string
+	Symbols    []Symbols
+}
+
+type Symbols struct {
+	Type  string
+	Value string
+	Name  string
+}
+
+// DetectionResults is a map of purl -> advisoryId -> []reachableSymbols
+type DetectionResults map[string]map[string]ReachableSymbolLocations
--- pkg/models/results.go
+++ pkg/models/results.go
@@ -43,11 +43,12 @@ type License string
 // Vulnerabilities grouped by package
 // TODO: rename this to be Package as it now includes license information too.
 type PackageVulns struct {
-	Package         PackageInfo        `json:"package"`
-	DepGroups       []string           `json:"dependency_groups,omitempty"`
-	Locations       []PackageLocations `json:"locations,omitempty"`
-	Vulnerabilities []Vulnerability    `json:"vulnerabilities,omitempty"`
-	Metadata        PackageMetadata    `json:"metadata,omitempty"`
+	Package                   PackageInfo        `json:"package"`
+	DepGroups                 []string           `json:"dependency_groups,omitempty"`
+	Locations                 []PackageLocations `json:"locations,omitempty"`
+	Vulnerabilities           []Vulnerability    `json:"vulnerabilities,omitempty"`
+	Metadata                  PackageMetadata    `json:"metadata,omitempty"`
+	AdvisoriesForReachability []string           `json:"reachability_advisories,omitempty"`
 }
 
 type AnalysisInfo struct {
