75b04551d739c5c9234f80d076504979dfbc5b6b
Marc Wieser <marc.wieser@datadoghq.com>
2025-04-10 18:27:32 +0200

    Updating goreleaser to target the new directory and fix the snapshot build script

--- scripts/build_snapshot.sh
+++ scripts/build_snapshot.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-goreleaser build --rm-dist --single-target --snapshot
+docker run \
+  --rm -e CGO_ENABLED=1 \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v `pwd`:/go/src/datadog-sbom-generator \
+  -w /go/src/datadog-sbom-generator ghcr.io/goreleaser/goreleaser-cross \
+  --clean --skip=validate --skip=publish --snapshot
