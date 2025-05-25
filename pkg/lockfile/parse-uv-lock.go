415f6be9d7b22c5275849a27e66256c6a36f5bc5
Ryan Bonham <ryan.bonham@corteva.com>
2025-05-25 12:47:58 -0400

    Improve root project detection
    Signed-off-by: Ryan Bonham <ryan.bonham@corteva.com>

--- pkg/lockfile/parse-uv-lock.go
+++ pkg/lockfile/parse-uv-lock.go
@@ -14,6 +14,7 @@ type UvLockPackageSource struct {
 	Registry string `toml:"registry,omitempty"`
 	Git      string `toml:"git,omitempty"`
 	Virtual  string `toml:"virtual,omitempty"`
+	Editable string `toml:"editable,omitempty"`
 }
 
 type UvLockPackageSdist struct {
@@ -99,63 +100,67 @@ func (e UvLockExtractor) Extract(f DepFile) ([]PackageDetails, error) {
 
 	//Convert array of packages to a map to make lookup faster.
 	parsedPackages := make(map[string]*UvLockPackage, len(parsedLockfile.Packages))
+	var rootPackage *UvLockPackage
 	for _, pkg := range parsedLockfile.Packages {
 		parsedPackages[pkg.Name] = pkg
+		if pkg.Source.Virtual == "." || pkg.Source.Editable == "." {
+			rootPackage = pkg
+		}
 	}
 
 	//This will hold packages we will return
 	var packages []PackageDetails
+	if rootPackage != nil {
+		//Get the Direct Dependencies
+		var directDependencies map[string]struct{}
+		if len(parsedLockfile.Packages) > 0 {
+			directDependencies = extractNames(rootPackage.Dependencies)
+		}
 
-	//Get the Direct Dependencies
-	var directDependencies map[string]struct{}
-	if len(parsedLockfile.Packages) > 0 {
-		directDependencies = extractNames(parsedLockfile.Packages[len(parsedLockfile.Packages)-1].Dependencies)
-	}
-
-	devDependencies := make(map[string]struct{})
-	if len(parsedLockfile.Packages) > 0 {
-		var pkg = parsedLockfile.Packages[len(parsedLockfile.Packages)-1]
-		for _, deps := range pkg.DevDependencies {
-			for _, dep := range deps {
-				devDependencies[dep.Name] = struct{}{}
+		devDependencies := make(map[string]struct{})
+		if len(parsedLockfile.Packages) > 0 {
+			for _, deps := range rootPackage.DevDependencies {
+				for _, dep := range deps {
+					devDependencies[dep.Name] = struct{}{}
+				}
 			}
 		}
-	}
 
-	for _, lockPackage := range parsedLockfile.Packages {
-		//Skip root package
-		if lockPackage.Source.Virtual == "." {
-			continue
+		for _, lockPackage := range parsedLockfile.Packages {
+			//Skip root package
+			if lockPackage.Source.Virtual == "." || lockPackage.Source.Editable == "." {
+				continue
+			}
+
+			_, commit, _ := strings.Cut(lockPackage.Source.Git, "#")
+
+			_, isDirect := directDependencies[lockPackage.Name]
+
+			_, isDevDependency := devDependencies[lockPackage.Name]
+			depGroups := []string{}
+			if isDevDependency {
+				depGroups = append(depGroups, "dev")
+			}
+
+			if !isDirect && !isDevDependency {
+				continue
+			}
+			pkgDetails := PackageDetails{
+				Name:           lockPackage.Name,
+				Version:        lockPackage.Version,
+				Commit:         commit,
+				PackageManager: models.Uv,
+				Ecosystem:      models.EcosystemPyPI,
+				IsDirect:       isDirect || isDevDependency,
+				Dependencies:   buildDependencies(lockPackage.Dependencies, parsedPackages, directDependencies),
+			}
+
+			if len(depGroups) > 0 {
+				pkgDetails.DepGroups = depGroups
+			}
+
+			packages = append(packages, pkgDetails)
 		}
-
-		_, commit, _ := strings.Cut(lockPackage.Source.Git, "#")
-
-		_, isDirect := directDependencies[lockPackage.Name]
-
-		_, isDevDependency := devDependencies[lockPackage.Name]
-		depGroups := []string{}
-		if isDevDependency {
-			depGroups = append(depGroups, "dev")
-		}
-
-		if !isDirect && !isDevDependency {
-			continue
-		}
-		pkgDetails := PackageDetails{
-			Name:           lockPackage.Name,
-			Version:        lockPackage.Version,
-			Commit:         commit,
-			PackageManager: models.Uv,
-			Ecosystem:      models.EcosystemPyPI,
-			IsDirect:       isDirect || isDevDependency,
-			Dependencies:   buildDependencies(lockPackage.Dependencies, parsedPackages, directDependencies),
-		}
-
-		if len(depGroups) > 0 {
-			pkgDetails.DepGroups = depGroups
-		}
-
-		packages = append(packages, pkgDetails)
 	}
 	return packages, nil
 }
