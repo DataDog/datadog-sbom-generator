a309ef5166a6e2192eb8756f18c025a431fa8fa1
Joshua Delgado <joshua.delgado@datadoghq.com>
2025-06-03 13:02:13 -0400

    Reverting to old script, we can update this in a separate PR

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 set -e
-env -u DD_API_KEY -u DD_APP_KEY go test ./... -coverpkg=./... -covermode=atomic -coverprofile coverage.out
+go test ./... -coverpkg=./... -covermode=atomic -coverprofile coverage.out
