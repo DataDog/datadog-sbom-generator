2bf71acea0e2a8a1182db1b754f2b2e3a56876ef
Ryan Coulter <ryan.coulter@datadoghq.com>
2025-06-06 09:34:08 -0400

    Comments

--- pkg/models/package_metadata_test.go
+++ pkg/models/package_metadata_test.go
@@ -6,17 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPackageMetadata_MergeDevDepndency(t *testing.T) {
+func TestPackageMetadata_MergeDevDependency(t *testing.T) {
 	t.Parallel()
 
 	t.Run("both dev dependencies, keep property", func(t *testing.T) {
 		t.Parallel()
 		metadata := PackageMetadata{
-			PackageManagerMetadata:  "npm",
+			PackageManagerMetadata:  string(NPM),
 			IsDevDependencyMetadata: "true",
 		}
 		other := PackageMetadata{
-			PackageManagerMetadata:  "npm",
+			PackageManagerMetadata:  string(NPM),
 			IsDevDependencyMetadata: "true",
 		}
 		result := metadata.Merge(other)
@@ -25,14 +25,29 @@ func TestPackageMetadata_MergeDevDepndency(t *testing.T) {
 		assert.True(t, isDevDep)
 	})
 
-	t.Run("one is dev other is not, remove property", func(t *testing.T) {
+	t.Run("existing package is dev and incoming package is not, remove property", func(t *testing.T) {
 		t.Parallel()
 		metadata := PackageMetadata{
-			PackageManagerMetadata:  "npm",
+			PackageManagerMetadata:  string(NPM),
 			IsDevDependencyMetadata: "true",
 		}
 		other := PackageMetadata{
-			PackageManagerMetadata: "npm",
+			PackageManagerMetadata: string(NPM),
+		}
+		result := metadata.Merge(other)
+
+		_, isDevDep := result[IsDevDependencyMetadata]
+		assert.False(t, isDevDep)
+	})
+
+	t.Run("incoming package is dev and existing package is not, remove property", func(t *testing.T) {
+		t.Parallel()
+		metadata := PackageMetadata{
+			PackageManagerMetadata: string(NPM),
+		}
+		other := PackageMetadata{
+			PackageManagerMetadata:  string(NPM),
+			IsDevDependencyMetadata: "true",
 		}
 		result := metadata.Merge(other)
 
